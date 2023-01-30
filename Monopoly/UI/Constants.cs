9b6c71038b711ff4664c2c0adfbdd3ac0e470318
Gabriel Ferrer <gabrielferrer@gmail.com>
2023-01-29 21:41:46 -0300

    Enhanced font size.

--- Monopoly/UI/Constants.cs
+++ Monopoly/UI/Constants.cs
@@ -8,8 +8,8 @@ namespace Monopoly.UI
 
         public const int BoardRows = 11;
         public const int BoardColumns = 11;
-        public const int BoardCellWidth = 140;
-        public const int BoardCellHeight = 140;
+        public const int BoardCellWidth = 160;
+        public const int BoardCellHeight = 160;
         public const int DefaultBoardCellRowSpan = 1;
         public const int DefaultBoardCellColumnSpan = 1;
         public const double BoardCellBorderThickness = 2;
